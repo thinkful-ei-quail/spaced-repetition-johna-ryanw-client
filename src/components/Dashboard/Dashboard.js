adf34577c7df70556cd7e272d3ecbde72dd4bb75
Ryan Whitmore <ryan@whitmorespace.com>
2020-10-05 09:23:41 -0700

    icon fix

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,7 +8,13 @@ export default class Dashboard extends Component {
 
         <p>Total Score is 28</p>
 
-        <p></p>
+        <h3>Words to Practice</h3>
+
+        <div>
+          <p>tormenta</p>
+          <p>correct answer count: 2</p>
+          <p>incorrect answer count: 8</p>
+        </div>
       </>
     );
   }
