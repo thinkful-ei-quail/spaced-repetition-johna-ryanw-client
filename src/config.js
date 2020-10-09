b788fc4a53b723961971d8fb0bc35e212d9c180d
Ryan Whitmore <ryan@whitmorespace.com>
2020-10-09 12:15:22 -0700

    deploying

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://sleepy-sands-61500.herokuapp.com',
+  API_ENDPOINT: 'https://sleepy-sands-61500.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token'
 };
